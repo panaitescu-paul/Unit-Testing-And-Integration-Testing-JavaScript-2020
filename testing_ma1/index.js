7655b6a26eab190657375c111276d915fac61107
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-03-27 13:23:55 +0100

    added javascript file addEventListener

--- testing_ma1/index.js
+++ testing_ma1/index.js
@@ -1,1 +1,20 @@
 // File to write the backend code for the html page
+
+let isInternetConnection = false;
+let totalPrice;
+const internetConnectionPrice = 200;
+const phoneLinePrice = 150;
+const motorolaPrice = 800;
+const iPhonePrice = 6000;
+const samsungPrice = 1000;
+const sonyPrice = 900;
+const huaweiPrice = 900;
+
+
+document.getElementById("chkInternetConnection").addEventListener("click", ()=> {
+    if (isInternetConnection)
+        isInternetConnection = false;
+    else
+        isInternetConnection = true;
+    console.log("isInternetConnection: ", isInternetConnection);
+});
